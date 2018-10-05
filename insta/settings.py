03fac031b9892b2d6807e12d350122d2b61ce488
brianngeno <bkn.ngeno@gmail.com>
2018-10-05 07:13:14 +0300

    modified timezones to africa nairobi and configured data

--- insta/settings.py
+++ insta/settings.py
@@ -32,6 +32,15 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'insta',
+        'USER': 'bknngeno',
+    'PASSWORD':'123',
+    }
+}
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -111,7 +120,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
