9bf2ead4e13efb06650cda5a9795fd2040921fdf
brianngeno <bkn.ngeno@gmail.com>
2018-10-05 07:40:54 +0300

    added the user model

--- instagram/models.py
+++ instagram/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    Image = models.ImageField(upload_to = 'images/')
+    Image_name = models.CharField(max_length =30)
+    Image_caption = models.TextField(max_length =40)
+    Likes = models.CharField(max_length =20)
+    Comments = models.TextField(max_length = 50)
+    Profile = models.ForeignKey
+
