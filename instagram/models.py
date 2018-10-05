5230570949449822d181ee6eab156bd33947212d
brianngeno <bkn.ngeno@gmail.com>
2018-10-06 00:29:08 +0300

    updated models

--- instagram/models.py
+++ instagram/models.py
@@ -5,10 +5,26 @@ class Image(models.Model):
     Image = models.ImageField(upload_to = 'images/')
     Image_name = models.CharField(max_length =30)
     Image_caption = models.TextField(max_length =40)
-    Likes = models.CharField(max_length =20)
-    Comments = models.TextField(max_length = 50)
+    Likes = models.CharField(max_length =20,blank =True)
     Profile = models.ForeignKey
+    pub_date = models.DateTimeField(auto_now_add=True, null=True)
+    comment = models.ForeignKey
 
 class Profile(models.Model):
     Profile_photo = models.ImageField(upload_to ='images/')
     Bio = models.TextField(max_length = 50)
+
+class Comment(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+
+    def save_comment(self):
+        self.save()
+
+    def delete_comment(self):
+        self.delete()
--- instagram/views.py
+++ instagram/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import Image,Profile
+
+
 # Create your views here.
 @login_required(login_url='/accounts/login/')
 def home_page(request):
@@ -17,3 +19,4 @@ def logout(request):
 def view_image(request):
     image = Image.objects.all()
     return render(request, 'main_pages/home.html',{"image":image})
+
