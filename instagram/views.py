3ee61aca5d6d151c9b67601a81ec152f500565e0
brianngeno <bkn.ngeno@gmail.com>
2018-10-05 17:01:46 +0300

    added admin panel

--- instagram/views.py
+++ instagram/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-
+from .models import Image,Profile
 # Create your views here.
 @login_required(login_url='/accounts/login/')
 def home_page(request):
-    return render(request, 'main_pages/home.html')
+    image = Image.objects.all()
+    return render(request, 'main_pages/home.html',{"image":image})
 
 def profile(request):
     return render(request, 'main_pages/profile.html')
 
 def logout(request):
     return render(request, 'main_pages/home.html')
+
+def view_image(request):
+    image = Image.objects.all()
+    return render(request, 'main_pages/home.html',{"image":image})
