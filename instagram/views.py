7a74d9fb0e96a020a26198bdd2d58347ff9ae04e
brianngeno <bkn.ngeno@gmail.com>
2018-10-07 06:19:02 +0300

    added an upload function

--- instagram/views.py
+++ instagram/views.py
@@ -16,6 +16,9 @@ def home_page(request):
 def profile(request):
     return render(request, 'main_pages/profile.html')
 
+    '''
+    editing user profile fillform & submission
+    '''
 @login_required(login_url='/accounts/login/')
 def edit(request):
     if request.method == 'POST':
@@ -28,15 +31,22 @@ def edit(request):
     else:
         fom = ProfileForm()
     return render(request, 'main_pages/edit_profile.html', {'uploadform':fom})
-
+    '''
+    logs out current user from account
+    '''
 def logout(request):
     return render(request, 'main_pages/home.html')
 
+    '''
+    returns all images uploaded
+    '''
 def view_image(request):
     image = Image.objects.all()
     return render(request, 'main_pages/home.html',{"image":image})
 
-
+    '''
+    searching for profile
+    '''
 def search(request):
     if 'search' in request.GET and request.GET['search']:
         search_term = request.GET.get('search')
@@ -47,3 +57,17 @@ def search(request):
     else:
         message = 'Enter term to search'
         return render(request, 'main_pages/search.html', {'message':message})
+
+@login_required(login_url='/accounts/login')
+def upload_image(request):
+    if request.method == 'POST':
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            upload = form.save(commit=False)
+            upload.profile = request.user
+            upload.save()
+            return redirect('home_page', username=request.user)
+    else:
+        form = ImageForm()
+    
+    return render(request, 'profile.html', {'form':form})
