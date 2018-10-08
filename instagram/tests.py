b3ae9e01c94e2ad0bdc450b4aec2d393da75b125
brianngeno <bkn.ngeno@gmail.com>
2018-10-08 12:40:39 +0300

    added a test class for image and set up instance and save test

--- instagram/tests.py
+++ instagram/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
-
+from .models import Profile,Image,Comment
+import datetime as dt
 # Create your tests here.
+class ImageTestCLass(TestCase):
+    '''
+    setup self instance of image
+    '''
+    def setUp(self):
+        self.post = Image(name='Music',description='music is a southing sound that moves souls')
+    
+    ''' 
+    test instance of image
+    '''
+    def test_instance(self):
+        self.assertTrue(isinstance(self.post,Image))
+
+
+    '''
+    test save image
+    '''
+
+    def test_save_image(self):
+        self.post.save_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images)>0)
