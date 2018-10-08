ea3110e389a3371e99cec47a2f4986a1a31a3653
brianngeno <bkn.ngeno@gmail.com>
2018-10-09 00:19:57 +0300

    added an instance of test cases for comments

--- instagram/tests.py
+++ instagram/tests.py
@@ -43,3 +43,23 @@ class profileTestCLass(TestCase):
         bio = Profile.objects.all()
         self.assertTrue(len(bio)>0)
 
+class CommentTestCase(TestCase):
+    '''
+    setup
+    '''
+    def setUp(self):
+        self.comment = Comment(name='lovely')
+    '''
+    test instance of comment
+    '''
+    def test_instance(self):
+        self.assertTrue(isinstance(self.comment,Commment))
+        '''
+        test for save instance of comment
+        '''
+    def test_save_comment(self):
+        self.comment.save_comment()
+        name= Comment.object.all()
+        sel.assertTrue(len(name)>0)
+    
+
