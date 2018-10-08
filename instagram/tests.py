ebf3c3bfd08262f0b51514637a5d3dce04da2ff4
brianngeno <bkn.ngeno@gmail.com>
2018-10-08 13:39:04 +0300

    ran test to check instance and save of image

--- instagram/tests.py
+++ instagram/tests.py
@@ -7,7 +7,7 @@ class ImageTestCLass(TestCase):
     setup self instance of image
     '''
     def setUp(self):
-        self.post = Image(name='Music',description='music is a southing sound that moves souls')
+        self.post = Image(Image_name='Music',Image_caption='music is a southing sound that moves souls')
     
     ''' 
     test instance of image
